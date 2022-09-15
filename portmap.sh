454d0a247a365fd9636d3892b2a3e950b2322598
Tejaswi Gowda <tejaswi@asu.edu>
2022-09-15 09:29:58 -0700

    ac

--- portmap.sh
+++ portmap.sh
@@ -1,1 +1,3 @@
-isudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8080
+sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to 8443
+
