d155d85e701dd8cd2528c82d1789fd5452b52e2c
root <root@ip-172-31-20-119.us-west-2.compute.internal>
2022-09-16 16:57:44 +0000

    https is working with greenlock@2

--- https-server/https.js
+++ https-server/https.js
@@ -1,18 +1,17 @@
-
-var app =  require('./server.js')
- 
-require("greenlock-express")
-    .init({
-        packageRoot: __dirname,
-        configDir: "./greenlock.d",
-        server: 'https://acme-v02.api.letsencrypt.org/directory',
- 
-        // contact for security and critical bug notices
-        maintainerEmail: 'tejaswi@asu.edu',
- 
-        // whether or not to run at cloudscale
-        cluster: false
-    })
-    // Serves on 80 and 443
-    // Get's SSL certificates magically!
-    .serve(app);
+var homeDir = require('path').join(require('os').homedir());
+require('greenlock-express').create({
+	  version: 'draft-11'
+	, server: 'https://acme-v02.api.letsencrypt.org/directory'
+	//, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'  // staging
+	, email: 'tejaswi@asu.edu'                                     // CHANGE THIS
+	, agreeTos: true
+	, approveDomains: [ 'mlserve.tejaswigowda.com','www.mlserve.tejaswigowda.com' ]              // CHANGE THIS
+	, store: require('greenlock-store-fs')
+	, configDir: homeDir
+	////, app: require('express')().use('/', function (req, res) {
+	////    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+	////    res.end('Hello, World!\n\n💚 🔒.js');
+	////  })
+	, app: require('./server.js')
+	////, communityMember: true
+	}).listen(8080, 8443);
