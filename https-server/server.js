8e1a025eb577b054608641001b2937a619ff8f37
sivareddy <sivahrez@gmail.com>
2022-09-20 15:27:22 -0700

    posenet 3d model

--- https-server/server.js
+++ https-server/server.js
@@ -11,17 +11,26 @@ var port = 8080;
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-
+var total = 0;
 
 app.post('/sendFrame', function(req, res){
-    axios.post("http://127.0.0.1:8000/sendFrame/", req.body ).then(function(response){
+    var startTime = performance.now();
+    axios.post("http://127.0.0.1:1234/sendFrame/", req.body ).then(function(response){
         res.end(JSON.stringify(response.data))
-    }).catch(err => console.error(err));      
+    }).catch(err => console.log("unable to connect to the django server"));
+    
+
 });
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
-app.listen(port);
+// app.listen(port);
 // // DO NOT DO app.listen() unless we're testing this directly
-// if (require.main === module) { app.listen(8080); }
-// // Instead do export the app:
-// else{ module.exports = app; }
+if (require.main === module) { app.listen(8080); }
+// Instead do export the app:
+else{ module.exports = app; }
+// var endTime = performance.now();
+//     total = total + (endTime - startTime);
+//     if (total >= 1000);{
+//         console.log(`no of frames = ${total/30}`)
+//         total = 0
+//     }
