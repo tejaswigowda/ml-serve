df82fbe0e7e55b76e85f63cc20ef770270f774b1
sivareddy <sivahrez@gmail.com>
2022-09-20 19:59:20 -0700

    updated html

--- https-server/server.js
+++ https-server/server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 var total = 0;
 
 app.post('/sendFrame', function(req, res){
-    var startTime = performance.now();
+    
     axios.post("http://127.0.0.1:1234/sendFrame/", req.body ).then(function(response){
         res.end(JSON.stringify(response.data))
     }).catch(err => console.log("unable to connect to the django server"));
@@ -27,10 +27,11 @@ console.log("Simple static server listening at http://" + hostname + ":" + port)
 // // DO NOT DO app.listen() unless we're testing this directly
 if (require.main === module) { app.listen(8080); }
 // Instead do export the app:
-else{ module.exports = app; }
+else{ module.exports = app;}
+// var startTime = performance.now();
 // var endTime = performance.now();
 //     total = total + (endTime - startTime);
 //     if (total >= 1000);{
 //         console.log(`no of frames = ${total/30}`)
 //         total = 0
-//     }
+//     
